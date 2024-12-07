009df4f2e8ebfc5620d287a3afc1de63565fdb03
Muhammed Can Çetin <68066318+mcancetin@users.noreply.github.com>
2024-12-07 21:33:31 +0300

    Fix import path for AudioButton component in search-result.jsx

--- src/components/common/search-result/search-result.jsx
+++ src/components/common/search-result/search-result.jsx
@@ -1,4 +1,4 @@
-import AudioButton from "@/components/audio-button";
+import AudioButton from "@/components/common/audio-button";
 import PropTypes from "prop-types";
 import { memo } from "react";
 
