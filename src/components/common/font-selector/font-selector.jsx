1c7be23431707cfc6e797f657ab76e832a6d9ded
Muhammed Can Çetin <68066318+mcancetin@users.noreply.github.com>
2024-12-07 20:37:23 +0300

    Add label and id attributes to FontSelector for improved accessibility

--- src/components/common/font-selector/font-selector.jsx
+++ src/components/common/font-selector/font-selector.jsx
@@ -4,16 +4,20 @@ import PropTypes from "prop-types";
 function FontSelector({ className }) {
     const { fontStyle, setFontStyle } = useTheme();
     return (
-        <select
-            name="fontStyle"
-            value={fontStyle}
-            onChange={(e) => setFontStyle(e.target.value)}
-            className={`bg-white px-2 text-dark-gray dark:bg-blackish dark:text-white ${className}`}
-        >
-            <option value="sans">Sans Serif</option>
-            <option value="serif">Serif</option>
-            <option value="mono">Mono</option>
-        </select>
+        <>
+            <label htmlFor="fontStyle"></label>
+            <select
+                id="fontStyle"
+                name="fontStyle"
+                value={fontStyle}
+                onChange={(e) => setFontStyle(e.target.value)}
+                className={`bg-white px-2 text-dark-gray dark:bg-blackish dark:text-white ${className}`}
+            >
+                <option value="sans">Sans Serif</option>
+                <option value="serif">Serif</option>
+                <option value="mono">Mono</option>
+            </select>
+        </>
     );
 }
 
